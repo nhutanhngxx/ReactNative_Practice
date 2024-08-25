db805f211c4dd6bc186202b2072156842d54d3a1
unknown <21139431.anh@student.iuh.edu.vn>
2024-08-25 19:13:49 +0700

    commit part2 #2

--- Lab01/Part2_2.js
+++ Lab01/Part2_2.js
@@ -1,14 +1,24 @@
 // Steven is still building his tip calculator, using the same rules as before:
 //     Tip 15% of the bill if the bill value is between 50 and 300,
 //     and if the value is different, the tip is 20%.
-
 // 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip,
-//     calculated based on the rules above
-//     (you can check out the code from first tip calculator challenge if you need to).
-//     Use the function type you like the most. Test the function using a bill value of 100
-
+// calculated based on the rules above
+// (you can check out the code from first tip calculator challenge if you need to).
+// Use the function type you like the most. Test the function using a bill value of 100
 // 2. And now let's use arrays! So create an array 'bills' containing the test data below.
-
 // 3.Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
-
 // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip.
+
+// 1. Viết hàm calcTip
+function calcTip(bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
+}
+
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+const tips = bills.map(calcTip);
+const total = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Bills: ", bills);
+console.log("Tips: ", tips);
+console.log("Total: ", total);
